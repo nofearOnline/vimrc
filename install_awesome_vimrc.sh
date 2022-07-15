6ebcf2ae721ca17c1b38c5243afcdf7919ee0d7e
Or Navon <navonor@gmail.com>
2022-07-15 18:22:40 +0300

    Update install_awesome_vimrc.sh
    adding mouse control over splits

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -12,6 +12,7 @@ source ~/.vim_runtime/vimrcs/basic.vim
 source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
 source ~/.vim_runtime/vimrcs/extended.vim
+set mouse=a
 try
   source ~/.vim_runtime/my_configs.vim
 catch
